14c66c89cba1be6bb0a5452d73a245de8911e4a6
Marco Silva <nelsonmarcosilva@gmail.com>
2023-08-25 23:15:13 -0300

    agregado pagina index login y cambios esteticos

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Inmobiliaria.Models;
+
+namespace Login.Controllers;
+
+public class LoginController : Controller
+{
+    private readonly ILogger<LoginController> _logger;
+
+    public LoginController(ILogger<LoginController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+
+
+        return View();
+    }
+
+   
+
+}
